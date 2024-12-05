632e9e1fb4355973ec72109fcbb194416203bd69
RiemannIntegrable <jmacunahz.01@gmail.com>
2024-12-05 00:08:13 -0500

    working on missing data

--- src/data/load.py
+++ src/data/load.py
@@ -3,13 +3,11 @@ from kaggle.api.kaggle_api_extended import KaggleApi
 
 download_path = os.path.expanduser("../../data/external")
 
-# Crear el directorio si no existe
 os.makedirs(download_path, exist_ok=True)
 
 api = KaggleApi()
 api.authenticate()
 
-# Descargar el dataset
 api.dataset_download_files("lainguyn123/student-performance-factors", path=download_path, unzip=True)
 
 print("Path to dataset files:", download_path)
